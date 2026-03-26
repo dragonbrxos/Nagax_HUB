a0035f93ff28252dd8696a89deb27d9943cef476
dragonbrxos <265870779+dragonbrxos@users.noreply.github.com>
2026-03-26 13:28:23 -0400

    Add real Items and Puzzles logic from base script

---
+++ Modules/Items/Puzzles.lua
@@ -0,0 +1,96 @@
+local ItemsModule = {}
+ItemsModule.AutoBuyLegendary = function()
+spawn(function()
+    while wait() do
+        if _G.AutoBuyLegendarySword then
+            pcall(function()
+                local args = {
+                    [1] = "LegendarySwordDealer",
+                    [2] = "1"
+                }
+                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+                local args = {
+                    [1] = "LegendarySwordDealer",
+                    [2] = "2"
+                }
+                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+                local args = {
+                    [1] = "LegendarySwordDealer",
+                    [2] = "3"
+                }
+                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+                if _G.AutoBuyLegendarySword_Hop and _G.AutoBuyLegendarySword and World2 then
+                    wait(10)
+                    Hop()
+                end
+            end)
+        end 
+    end
+end)
+end
+ItemsModule.AutoHakiColor = function()
+Farm:Seperator("Enchancement Colour")
+
+ColorHaki = Farm:Label("")
+local KuyKoben = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("ColorsDealer", "1")
+spawn(function()
+    pcall(function()
+        while wait() do
+            if game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer("ColorsDealer", "1") then
+                ColorHaki:Set(KuyKoben)
+            else
+                ColorHaki:Set("Not Found HakiDealer")
+            end
+        end
+    end)
+end)
+
+Farm:Toggle("Auto Buy Enchancement Colour", false, function(value)
+    _G.AutoBuyEnchancementColour = valu
+end)
+spawn(function()
+    while wait() do
+        if _G.AutoBuyEnchancementColour then
+            local args = {
+                [1] = "ColorsDealer",
+                [2] = "2"
+            }
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+            if _G.AutoBuyEnchancementColour_Hop and _G.AutoBuyEnchancementColour and not World1 then
+                wait(10)
+                Hop()
+            end
+        end 
+    end
+end)
+end
+ItemsModule.AutoMaterial = function()
+Farm:Seperator("Auto Material")
+
+Farm:Toggle("Auto Farm Radioactive", false, function(value)
+    Radioactive = value
+    StopTween(Radioactive)
+end)
+local MaterialsPos1 = CFrame.new(-507.7895202636719, 72.99479675292969, -126.45632934570312)
+spawn(function()
+    while wait() do
+        if Radioactive and World2 then
+            pcall(function()
+                if game:GetService("Workspace").Enemies:FindFirstChild("Factory Staff") then
+                    for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+                        if v.Name == "Factory Staff" then
+                            if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
+                                repeat task.wait()
+                                    AutoHaki()
+                                    EquipWeapon(_G.SelectWeapon)
+                                    v.HumanoidRootPart.CanCollide = false
+                                    v.Humanoid.WalkSpeed = 0
+                                    v.Head.CanCollide = false 
+                                    MakoriGayMag = true
+                                    PosGay = v.HumanoidRootPart.CFrame
+                                    topos(v.HumanoidRootPart.CFrame * Pos)
+                                    game:GetService("VirtualUser"):CaptureController()
+                                    game:GetService("VirtualUser"):Button1Down(Vector2.new(1280,672))
+                                until not Radioactive or not v.Parent or v.Humanoid.Health <= 0
+end
+return ItemsModule
